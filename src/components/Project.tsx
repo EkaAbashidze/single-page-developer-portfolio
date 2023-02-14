660773fbf02d73621dfbf68ec81b7bf0cafbfb6b
Eka <eka.abashidze@gmail.com>
2023-02-14 16:32:25 +0400

    project component setup, passed down imgs, titles, arrays as props

---
+++ src/components/Project.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Project({photo, title, arr}: {photo: string, title: string, arr: string[]}) {
+  return (
+    <div className="text-white max-w-[343px] mx-auto pb-[40px]">
+      <img src={photo} alt="" className='cursor-pointer'/>
+      <h2 className=' font-grotesk-bold text-[24px] leading-[32px] pt-[20px] cursor-pointer'>{title}</h2>
+
+      <div className='flex gap-x-[18px] text-grey font-grotesk-medium text-[18px] leading-[28px] pt-[7px] pb-[20px]' >
+
+
+        {arr.map(stack => <h3>{stack}</h3>)}    
+
+
+      </div>
+
+      <div className="flex gap-x-8">
+        <button className="flex flex-col items-center gap-y-[10px]">
+          <h2 className="font-grotesk-bold text-base leading-[26px] tracking-[2.26px]">
+            VIEW PROJECT
+          </h2>
+          <div className="w-full h-[2px] bg-green"></div>
+        </button>
+
+        <button className="flex flex-col items-center gap-y-[10px]">
+          <h2 className="font-grotesk-bold text-base leading-[26px] tracking-[2.26px]">
+            VIEW CODE
+          </h2>
+          <div className="w-full h-[2px] bg-green"></div>
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,22 +1,41 @@
 import React from 'react'
+import Project from './Project';
+import Thumb1 from "../assets/images/thumbnail-project-1-small.webp"
+import Thumb2 from "../assets/images/thumbnail-project-2-small.webp"
+import Thumb3 from "../assets/images/thumbnail-project-3-small.webp"
+import Thumb4 from "../assets/images/thumbnail-project-4-small.webp"
+import Thumb5 from "../assets/images/thumbnail-project-5-small.webp"
+import Thumb6 from "../assets/images/thumbnail-project-6-small.webp"
 
 export default function Projects() {
+
+
+  const stacks: string[] = ["HTML", "CSS"];
+  const stacksAll: string[] = ["HTML", "CSS", "JAVASCRIPT"];
+
+
   return (
     <>
-    <div className="text-white pb-[40px] flex justify-between max-w-[343px] mx-auto items-center">
-      <h1 className='font-grotesk-bold text-[40px] leading-[40px] tracking-[-1.14px]' >Projects</h1>
-      <button className='flex flex-col items-center gap-y-[10px]'>
-        <h2 className='font-grotesk-bold text-base leading-[26px] tracking-[2.26px]' >CONTACT ME</h2>
-        <div className='w-[120px] h-[2px] bg-green' ></div>
-      </button>
+      <div className="text-white pb-[40px] flex justify-between max-w-[343px] mx-auto items-center">
+        <h1 className="font-grotesk-bold text-[40px] leading-[40px] tracking-[-1.14px]">
+          Projects
+        </h1>
+        <button className="flex flex-col items-center gap-y-[10px]">
+          <h2 className="font-grotesk-bold text-base leading-[26px] tracking-[2.26px]">
+            CONTACT ME
+          </h2>
+          <div className="w-[120px] h-[2px] bg-green"></div>
+        </button>
       </div>
-
 
       <div>
-        
+        <Project photo={Thumb1} title="DESIGN PORTFOLIO" arr={stacks} />
+        <Project photo={Thumb2} title="E-LEARNING LANDING PAGE" arr={stacks}/>
+        <Project photo={Thumb3} title="TODO WEB APP" arr={stacksAll}/>
+        <Project photo={Thumb4} title="ENTERTAINMENT WEB APP" arr={stacksAll}/>
+        <Project photo={Thumb5} title="MEMORY GAME" arr={stacksAll}/>
+        <Project photo={Thumb6} title="ART GALLERY SHOWCASE" arr={stacksAll}/>
       </div>
-
-
     </>
   );
 }
